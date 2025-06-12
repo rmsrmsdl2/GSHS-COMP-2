a1f8c0cc01d2787fd38b6675d854cc24665671d5
Jaemin Kwak <kwakjaem0924@gmail.com>
2025-06-12 13:56:30 +0900

    Update 그래프(하)(H-K).cpp

--- 그래프(하)(H-K).cpp
+++ 그래프(하)(H-K).cpp
@@ -1,1 +1,55 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+int n,m;
+vector<vector<int>> graph(101);
+vector<bool> visit(101,false);
+void dfs(int node)
+{
+    visit[node]=true;
+    cout<<node<<" ";
+    for (auto next:graph[node]){
+        if (!visit[next]){
+            dfs(next);
+        }
+    }
+}
+int main()
+{
+    cin>>n>>m;
+    while(m--){
+        int x,y;
+        cin>>x>>y;
+        graph[x].push_back(y);
+        graph[y].push_back(x);
+    }
+    for (int i=1;i<=n;i++){
+        sort(graph[i].begin(),graph[i].end());
+    }
+    dfs(1);
+} // PROB H 깊이우선탐색 I
+#include<bits/stdc++.h>
+using namespace std;
+int n,m;
+vector<vector<int>> graph(2001);
+vector<bool> visit(2001,false);
+void dfs(int node)
+{
+    visit[node]=true;
+    cout<<node<<" ";
+    for (auto next:graph[node]){
+        if (!visit[next]){
+            dfs(next);
+        }
+    }
+}
+int main()
+{
+    cin>>n>>m;
+    while(m--){
+        int x,y;
+        cin>>x>>y;
+        graph[x].push_back(y);
+        graph[y].push_back(x);
+    }
+    dfs(1);
+} // PROB I 깊이우선탐색 II

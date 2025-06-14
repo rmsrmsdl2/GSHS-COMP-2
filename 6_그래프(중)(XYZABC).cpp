d9229f27227fa14ee479ef0c19f6d39f15248281
Jaemin Kwak <kwakjaem0924@gmail.com>
2025-06-14 23:55:01 +0900

    Update 6_그래프(중)(XYZABC).cpp

--- 6_그래프(중)(XYZABC).cpp
+++ 6_그래프(중)(XYZABC).cpp
@@ -39,3 +39,27 @@ int main()
     }
     cout<<ans;
 } // PROB Y 위성 사진 
+
+
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+    ll n,ans=1,mod=998244353;
+    ll deg[300100]={},fac[300100]={1};
+    cin>>n;
+    for (int i=1;i<=n;i++){
+        fac[i]=(fac[i-1]*i)%mod;
+    }
+    for (int i=1;i<=n-1;i++){
+        ll x,y;
+        cin>>x>>y;
+        deg[x]++;
+        deg[y]++;
+    }
+    for (int i=1;i<=n;i++){
+        ans=(ans*fac[(i==1)?deg[i]:deg[i]-1])%mod;
+    }
+    cout<<ans;
+} // PROB Z 트리의 깊이우선탐색의 개수 

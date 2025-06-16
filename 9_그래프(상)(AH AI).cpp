029ce85ee57bd2419bb8bf3926b01dd73a4e9698
Jaemin Kwak <kwakjaem0924@gmail.com>
2025-06-16 20:44:48 +0900

    Update 9_그래프(상)(AH AI).cpp

--- 9_그래프(상)(AH AI).cpp
+++ 9_그래프(상)(AH AI).cpp
@@ -3,21 +3,17 @@ AH는 다른 분들의 코드가 훨씬 좋은 것 같아서 그거 참고하시
 
 #include<bits/stdc++.h>
 using namespace std;
-
 int h,w,ans;
 char board[50][51];
 int dist[50][50];
 int dx[4]={1,-1,0,0},dy[4]={0,0,1,-1};
-
 int main(){
     cin>>h>>w;
     for(int i=0;i<h;i++) cin>>board[i];
-
     deque<pair<int,int>> qu;
     memset(dist,0x3f,sizeof(dist));
     dist[h-1][w-1]=1;
     qu.push_back({h-1,w-1});
-
     while(!qu.empty()){
         int x=qu.front().first, y=qu.front().second;
         qu.pop_front();
@@ -33,7 +29,6 @@ int main(){
             }
         }
     }
-
     int res=0;
     for(int i=0;i<h;i++){
         for(int j=0;j<w;j++){
@@ -42,4 +37,4 @@ int main(){
     }
     cout<<res<<"\n";
     return 0;
-} // PROB AI
+} // PROB AI 토끼와 여우
